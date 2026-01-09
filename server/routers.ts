484335bd5f732e58956425fc5d7035b394703b90
Quézia Jesuino <quezitabjq@gmail.com>
2026-01-08 22:02:43 -0400

    Remover a definição da constante START_API_BASE no arquivo routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -5,7 +5,7 @@ import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, router } from "./_core/trpc";
 
 const PUBLIC_API_BASE = "https://portal.iecg.com.br/public";
-const START_API_BASE = "https://portal.iecg.com.br/start";
+
 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
